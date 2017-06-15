c55cd5fe8a0fef70daf8c8c69b5f45f3609b6e60
jrdisla <20111614@ce.pucmm.edu.do>
2017-06-15 16:50:00 -0400

    Ultimos detalles

--- src/main/java/Clases/Main.java
+++ src/main/java/Clases/Main.java
@@ -9,6 +9,7 @@ import spark.Spark;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.prefs.Preferences;
 
 import static spark.debug.DebugScreen.enableDebugScreen;
 
@@ -18,42 +19,26 @@ import static spark.debug.DebugScreen.enableDebugScreen;
 public class Main {
     public static void main(String[] args) {
 
-        UsuarioDao usuarioDao = new UsuarioDao();
-        TagDao tagDao = new TagDao();
-        ArticuloDao articuloDao = new ArticuloDao();
-        CommentDao commentDao = new CommentDao();
-
-        Usuario usuario = new Usuario("admin", "admin", "admin", true, true);
-        Tag tag = new Tag(1,"holaa");
-
-        ArrayList<Tag> tags = new ArrayList<>();
-        ArrayList<Comentario> comentarios = new ArrayList<>();
-
-      //  Articulo articulo = new Articulo(1, "Hola", "Si", usuario.getUsername(),"02", comentarios,tags);
-     //   Articulo articulo1 = new Articulo(2, "Hola", "Si", usuario.getUsername(),"02", comentarios,tags);
-      //  Articulo articulo2 = new Articulo(3, "Hola", "Si", usuario.getUsername(),"02", comentarios,tags);
-     //   Articulo articulo3 = new Articulo(4, "Hola", "Si", usuario.getUsername(),"02", comentarios,tags);
-     //   Articulo articulo4 = new Articulo(5, "Hola", "Si", usuario.getUsername(),"02", comentarios,tags);
-
-    //    Comentario comentario = new Comentario(1, "hola que tal", usuario, articulo);
-
-      // comentarios.add(comentario);
-
-     //  tags.add(tag);
-
-     // usuarioDao.inserIntoUsers(usuario);
- //     tagDao.inserIntoTags(tag);
- //      articuloDao.inserIntoArticulos(articulo);
- //      articuloDao.inserIntoArticulos(articulo1);
-   //     articuloDao.inserIntoArticulos(articulo2);
-   //     articuloDao.inserIntoArticulos(articulo3);
-  //      articuloDao.inserIntoArticulos(articulo4);
-  //   commentDao.inserIntoTags(comentario);
-
+        Preferences user_pre = Preferences.userRoot();
+        Boolean isFirstRun = user_pre.getBoolean("first_run", true);
+        System.out.println(isFirstRun);
+        if (isFirstRun) {
+            System.out.println("running for the first time");
+            UsuarioDao usuarioDao = new UsuarioDao();
+            TagDao tagDao = new TagDao();
+            ArticuloDao articuloDao = new ArticuloDao();
+            CommentDao commentDao = new CommentDao();
+            Usuario usuario = new Usuario("admin", "admin", "admin", true, true);
+            usuarioDao.inserIntoUsers(usuario);
+            user_pre.putBoolean("first_run", false);
+        }
         Spark.staticFileLocation("/public");
         enableDebugScreen();
+        new ManejadorTemp().startApp();
 
-       new ManejadorTemp().startApp();
+
+
+
 
     }
 }
